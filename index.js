1e76813cc8fbef9a2159e5735143687b47104f79
Michael Zetterberg fd. Lopez <michael@weahead.se>
2018-11-12 03:35:12 +0100

    First version.

---
+++ index.js
@@ -0,0 +1,71 @@
+const {parse, URL} = require('url');
+const fetch = require('node-fetch');
+const {json} = require('micro');
+
+// Based on zeit/micro-proxy
+// See: https://github.com/zeit/micro-proxy/blob/133e645bc675dc89405a2afcd988a40d7c15e034/index.js
+async function proxyRequest (dest, body, req, res) {
+  const url = new URL(dest);
+  const proxyRes = await
+    fetch(url, {
+      method: req.method,
+      headers: Object.assign({}, req.headers, {'Content-Type': 'application/json', 'host': url.host}),
+      body,
+      compress: false
+    });
+
+  res.statusCode = proxyRes.status;
+  const headers = proxyRes.headers.raw();
+  for (const key of Object.keys(headers)) {
+    res.setHeader(key, headers[key]);
+  }
+
+  proxyRes.body.pipe(res);
+  proxyRes.body.on('error', (err) => {
+    console.error(`Error on proxying url: ${newUrl}`);
+    console.error(err.stack);
+    res.end();
+  });
+
+  req.on('abort', () => {
+    proxyRes.body.destroy();
+  });
+}
+
+module.exports = async (req, res) => {
+  try {
+    const query = parse(req.url, true).query;
+    const dest = query.url;
+
+    if (!dest) {
+      res.statusCode = 404;
+      res.end('Not found');
+      return
+    }
+
+    let body;
+    if (query.ref) {
+      const data = await json(req);
+      const {object_attributes: attr} = data;
+      if (!attr || !attr.ref) {
+        res.statusCode = 400;
+        res.end('Bad request');
+        return;
+      }
+
+      if (query.ref !== attr.ref) {
+        res.statusCode = 202;
+        res.end('Accepted');
+        return;
+      }
+      body = JSON.stringify(data);
+    } else {
+      body = req;
+    }
+
+    await proxyRequest(dest, body, req, res);
+  } catch (err) {
+    console.error(err.stack);
+    res.end();
+  }
+};
